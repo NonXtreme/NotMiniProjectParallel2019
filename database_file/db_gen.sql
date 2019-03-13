ad6fe199675e511c09c04e7e72502bcb3715d4d5
Chaiyaphon Yaembane <nonscinus@gmail.com>
2019-03-13 20:08:12 +0700

    Add database gen script

---
+++ database_file/db_gen.sql
@@ -0,0 +1,30 @@
+drop schema if exists parallel_db;
+create schema parallel_db;
+use parallel_db;
+create table user_(
+  user_ID varchar(6) primary key,
+  username varchar(10),
+  passwd varchar(20)
+);
+
+create table group_(
+  group_ID varchar(6) primary key,
+  groupname varchar(10)
+);
+
+create table join_(
+  user_ID varchar(6),
+  group_ID varchar(6),
+  constraint PK primary key (user_ID, group_ID),
+  constraint FK_join1 foreign key (user_ID)  references user_(user_ID),
+  constraint FK_join2 foreign key (group_ID) references group_(group_ID)
+);
+create table message(
+  msg_ID varchar(6) primary key,
+  content varchar(200),
+  time_stamp timestamp,
+  user_ID varchar(6),
+  group_ID varchar(6),
+  constraint FK_msg1 foreign key (user_ID)  references user_(user_ID),
+  constraint FK_msg2 foreign key (group_ID) references group_(group_ID)
+);
