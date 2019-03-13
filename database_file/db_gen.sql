643db52d82d4bc2c9b38207360442e6119a500e8
Chaiyaphon Yaembane <nonscinus@gmail.com>
2019-03-13 21:14:16 +0700

    Edit database gen script

--- database_file/db_gen.sql
+++ database_file/db_gen.sql
@@ -2,19 +2,19 @@ drop schema if exists parallel_db;
 create schema parallel_db;
 use parallel_db;
 create table user_(
-  user_ID varchar(6) primary key,
+  user_ID int primary key auto_increment,
   username varchar(10),
   passwd varchar(20)
 );
 
 create table group_(
-  group_ID varchar(6) primary key,
+  group_ID int primary key auto_increment,
   groupname varchar(10)
 );
 
 create table join_(
-  user_ID varchar(6),
-  group_ID varchar(6),
+  user_ID int,
+  group_ID int,
   constraint PK primary key (user_ID, group_ID),
   constraint FK_join1 foreign key (user_ID)  references user_(user_ID),
   constraint FK_join2 foreign key (group_ID) references group_(group_ID)
@@ -23,8 +23,8 @@ create table message(
   msg_ID varchar(6) primary key,
   content varchar(200),
   time_stamp timestamp,
-  user_ID varchar(6),
-  group_ID varchar(6),
+  user_ID int,
+  group_ID int,
   constraint FK_msg1 foreign key (user_ID)  references user_(user_ID),
   constraint FK_msg2 foreign key (group_ID) references group_(group_ID)
 );
