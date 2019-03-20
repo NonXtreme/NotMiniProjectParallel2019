06b2913e4a88e4818fc0ea691eaf87953d4dd1a5
Non <smartnon2009@hotmail.com>
2019-03-20 17:14:26 +0700

    A lot of change...
    - Revert to old mysql query method
    - Remove user_ID and group_id
    - remove handleDisconnect()

--- database_file/db_gen.sql
+++ database_file/db_gen.sql
@@ -2,29 +2,26 @@ drop schema if exists parallel_db;
 create schema parallel_db;
 use parallel_db;
 create table user_(
-  user_ID int primary key auto_increment,
-  username varchar(10),
-  passwd varchar(20)
+  username varchar(10) primary key
 );
 
 create table group_(
-  group_ID int primary key auto_increment,
-  groupname varchar(10)
+  groupname varchar(10) primary key
 );
 
 create table join_(
-  user_ID int,
-  group_ID int,
-  constraint PK primary key (user_ID, group_ID),
-  constraint FK_join1 foreign key (user_ID)  references user_(user_ID),
-  constraint FK_join2 foreign key (group_ID) references group_(group_ID)
+  username varchar(10),
+  groupname varchar(10),
+  constraint PK primary key (username, groupname),
+  constraint FK_join1 foreign key (username)  references user_(username),
+  constraint FK_join2 foreign key (groupname) references group_(groupname)
 );
 create table message(
   msg_ID int(6) primary key auto_increment,
   content varchar(200),
   time_stamp timestamp,
-  user_ID int,
-  group_ID int,
-  constraint FK_msg1 foreign key (user_ID)  references user_(user_ID),
-  constraint FK_msg2 foreign key (group_ID) references group_(group_ID)
+  username varchar(10),
+  groupname varchar(10),
+  constraint FK_msg1 foreign key (username)  references user_(username),
+  constraint FK_msg2 foreign key (groupname) references group_(groupname)
 );
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(http);
 const mysql = require('mysql');
 const config = require('./config/dbconnect');
 const path = require('path');
-const connectionPool = mysql.createPool({
+const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: config.database.user,
@@ -44,206 +44,209 @@ io.on('connection', function (socket) {
 
 
   socket.on('createGroup', function (data,callback) {
-    console.log("Creating group " + data.groupID + " " + data.groupname);
-    let query = 'INSERT INTO group_(group_ID,groupname) values(?,?)';
+    console.log("Creating group " + data.groupName);
+    let query = 'INSERT INTO group_(groupname) values(?)';
 
+  //   connectionPool.getConnection(function (err, connection) {
+  //     connection.query('START TRANSACTION', function (err, rows) {
+  //       connection.query(query, [data.groupName, data.groupname], (error, result) => {
+  //         if (error) throw error;
+  //         console.log(result);
+  //         callback({status:"SUCCESS", result:result});
+  //         console.log('create group success!!!');
+  //       });
+  //     });
+  //     connection.query('COMMIT', function (err, rows) {
+  //       connection.release();
+  //     });
+  //   });
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [data.groupID, data.groupname], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          callback({status:"SUCCESS", result:result});
-          console.log('create group success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
+    connection.query(query, [data.groupName], (error, result) => {
+      if (error) throw error
+      console.log(result);
+      callback({status:"SUCCESS", result:result});
+      console.log('create group success!!!');
     });
-
-    // connection.query(query, [data.groupID, data.groupname], (error, result) => {
-    //   if (error) throw error
-    //   console.log('create group success!!!');
-    // });
   })
 
   socket.on('login', function (data,callback) {
-    console.log("Logging in " + data.userID);
-    let query = 'INSERT INTO join_(user_ID,group_ID) values(?,?)';
+    console.log("Logging in " + data.userName);
+    let query = 'INSERT INTO join_(username) values(?)';
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [data.userID], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          callback({status:"SUCCESS", result:result});
-          console.log('Login success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
-    });
-
-    // connection.query(query, [data.userID], (error, result) => {
-    //   if (error) throw error
-    //   console.log('Login success!!!');
+    // connectionPool.getConnection(function (err, connection) {
+    //   onsole.log("Bftrans");
+    //   connection.query('START TRANSACTION', function (err, rows) {
+    //     connection.query(query, [data.userName], (error, result) => {
+    //       if (error) throw error;
+    //       console.log(result);
+    //       callback({status:"SUCCESS", result:result});
+    //       console.log('Login success!!!');
+    //     });
+    //   });
+    //   connection.query('COMMIT', function (err, rows) {
+    //     connection.release();
+    //   });
     // });
+
+    connection.query(query, [data.userName], (error, result) => {
+      if (error) throw error
+      console.log(result);
+      callback({status:"SUCCESS", result:result});
+      console.log('Login success!!!');
+    });
   })
 
   socket.on('joinGroup', function (data,callback) {
-    console.log("Creating group " + data.userID + " " + data.groupID);
-    let query = 'INSERT INTO join_(user_ID,group_ID) values(?,?)';
+    console.log("Joining group " + data.userName + " " + data.groupName);
+    let query = 'INSERT INTO join_(username,groupname) values(?,?)';
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [data.userID, data.groupID], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          //TODO socket room?
-          socket.join(data.groupID);
-          callback({status:"SUCCESS", result:result});
-          // io.to(data.groupID).emit("joinGroup", data);
-          console.log('Join group success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
-    });
-
-
-    // connection.query(query, [data.userID, data.groupID], (error, result) => {
-    //   if (error) throw error
-    //   //TODO socket room?
-    //   socket.join(data.groupID);
-    //   io.to(data.groupID).emit("joinGroup", data);
-    //   console.log('Join group success!!!');
+    // connectionPool.getConnection(function (err, connection) {
+    //   connection.query('START TRANSACTION', function (err, rows) {
+    //     connection.query(query, [data.userName, data.groupName], (error, result) => {
+    //       if (error) throw error;
+    //       console.log(result);
+    //       //TODO socket room?
+    //       socket.join(data.groupName);
+    //       callback({status:"SUCCESS", result:result});
+    //       // io.to(data.groupName).emit("joinGroup", data);
+    //       console.log('Join group success!!!');
+    //     });
+    //   });
+    //   connection.query('COMMIT', function (err, rows) {
+    //     connection.release();
+    //   });
     // });
+
+    connection.query(query, [data.userName, data.groupName], (error, result) => {
+      if (error) throw error
+      console.log(result);
+      socket.join(data.groupName);
+      callback({status:"SUCCESS", result:result});
+      console.log('Join group success!!!');
+    });
   })
 
   socket.on('leaveGroup', function (data,callback) {
-    console.log("Leaving group " + data.userID + " " + data.groupID);
-    let query = 'DELETE FROM join_ WHERE user_ID=? and group_ID=?';
+    console.log("Leaving group " + data.userName + " " + data.groupName);
+    let query = 'DELETE FROM join_ WHERE username=? and groupname=?';
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [data.userID, data.groupID], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          //TODO socket room?
-          socket.leave(data.groupID);
-          callback({status:"SUCCESS", result:result});
-          //io.to(data.groupID).emit("leaveGroup", data);
-          console.log('Leave group success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
-    });
-
-    // connection.query(query, [data.userID, data.groupID], (error, result) => {
-    //   if (error) throw error
-    //   //TODO socket room?
-    //   io.to(data.groupID).emit("leaveGroup", data);
-    //   console.log('Leave group success!!!');
+    // connectionPool.getConnection(function (err, connection) {
+    //   connection.query('START TRANSACTION', function (err, rows) {
+    //     connection.query(query, [data.userName, data.groupName], (error, result) => {
+    //       if (error) throw error;
+    //       console.log(result);
+    //       //TODO socket room?
+    //       socket.leave(data.groupName);
+    //       callback({status:"SUCCESS", result:result});
+    //       //io.to(data.groupName).emit("leaveGroup", data);
+    //       console.log('Leave group success!!!');
+    //     });
+    //   });
+    //   connection.query('COMMIT', function (err, rows) {
+    //     connection.release();
+    //   });
     // });
+
+    connection.query(query, [data.userName, data.groupName], (error, result) => {
+      if (error) throw error
+      console.log(result);
+      socket.leave(data.groupName);
+      callback({status:"SUCCESS", result:result});
+      console.log('Leave group success!!!');
+    });
   })
 
   socket.on('getAllGroup', function (data,callback) {
     console.log("Getting all groups ");
-    let searchResults = [];
+   // let searchResults = [];
     let query = 'select * from group_';
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          searchResults = result;
-          callback({status:"SUCCESS", result:result});
-          console.log('get all group success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
-    });
-
-    // connection.query(query, [data.userID, data.groupID], (error, result) => {
-    //   if (error) throw error
-    //   searchResults = result;
-    //   //TODO socket room?
-    //   io.to(data.groupID).emit("getAllGroup", JSON.parse(JSON.stringify(searchResults)));
-    //   console.log('get all group success!!!');
+    // connectionPool.getConnection(function (err, connection) {
+    //   connection.query('START TRANSACTION', function (err, rows) {
+    //     connection.query(query, [], (error, result) => {
+    //       if (error) throw error;
+    //       console.log(result);
+    //       searchResults = result;
+    //       callback({status:"SUCCESS", result:result});
+    //       console.log('get all group success!!!');
+    //     });
+    //   });
+    //   connection.query('COMMIT', function (err, rows) {
+    //     connection.release();
+    //   });
     // });
+
+    connection.query(query, [], (error, result) => {
+      if (error) throw error
+      console.log(result);
+      callback({status:"SUCCESS", result:result});
+      console.log('get all group success!!!');
+    });
   })
 
   socket.on('getJoinedGroup', function (data,callback) {
-    console.log("Getting groups " + data.userID);
+    console.log("Getting joined groups " + data.userName);
     // let searchResults = [];
-    let query = 'select join_.group_ID,group_.groupname from join_,group_ where user_ID=?';
+    let query = 'select join_.groupname from join_ where username=?';
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [data.userID], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          // searchResults = result;
-          callback({status:"SUCCESS", result:result});
-          //TODO socket room?
-          // io.to(data.groupID).emit("getJoinedGroup", JSON.parse(JSON.stringify(searchResults)));
-          console.log('get joined group success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
-    });
-
-
-    // connection.query(query, [data.userID], (error, result) => {
-    //   if (error) throw error
-    //   searchResults = result;
-    //   //TODO socket room?
-    //   io.to(data.groupID).emit("getJoinedGroup", JSON.parse(JSON.stringify(searchResults)));
-    //   console.log('get joined group success!!!');
+    // connectionPool.getConnection(function (err, connection) {
+    //   connection.query('START TRANSACTION', function (err, rows) {
+    //     connection.query(query, [data.userName], (error, result) => {
+    //       if (error) throw error;
+    //       console.log(result);
+    //       // searchResults = result;
+    //       callback({status:"SUCCESS", result:result});
+    //       //TODO socket room?
+    //       // io.to(data.groupName).emit("getJoinedGroup", JSON.parse(JSON.stringify(searchResults)));
+    //       console.log('get joined group success!!!');
+    //     });
+    //   });
+    //   connection.query('COMMIT', function (err, rows) {
+    //     connection.release();
+    //   });
     // });
+
+
+    connection.query(query, [data.userName], (error, result) => {
+      if (error) throw error
+      console.log(result);
+      callback({status:"SUCCESS", result:result});
+      console.log('get joined group success!!!');
+    });
   })
 
   socket.on('sendMessage', function (msg,callback) {
-    //have msg.content msg.username msg.groupname msg.userID msg.group_ID
+    //have msg.content msg.userName msg.groupname msg.userName msg.group_ID
 
     // get time
     var mysqlTimestamp = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
 
     //inset to table 
-    let query = 'INSERT INTO message(content, time_stamp, user_ID, group_ID) VALUES (?,?,?,?)'
+    let query = 'INSERT INTO message(content, time_stamp, username, groupname) VALUES (?,?,?,?)'
 
-    connectionPool.getConnection(function (err, connection) {
-      connection.query('START TRANSACTION', function (err, rows) {
-        connection.query(query, [msg.message, mysqlTimestamp, msg.userID, msg.groupID], (error, result) => {
-          if (error) throw error;
-          console.log(result);
-          callback({status:"SUCCESS", result:result});
-          console.log('send message success!!!');
-        });
-      });
-      connection.query('COMMIT', function (err, rows) {
-        connection.release();
-      });
-    });
+    // connectionPool.getConnection(function (err, connection) {
+    //   connection.query('START TRANSACTION', function (err, rows) {
+    //     connection.query(query, [msg.message, mysqlTimestamp, msg.userName, msg.groupName], (error, result) => {
+    //       if (error) throw error;
+    //       console.log(result);
+    //       callback({status:"SUCCESS", result:result});
+    //       console.log('send message success!!!');
+    //     });
+    //   });
+    //   connection.query('COMMIT', function (err, rows) {
+    //     connection.release();
+    //   });
+    // });
 
 
 
-    // connection.query(query, [msg.message, mysqlTimestamp, msg.userID, msg.groupID], function (err, results) {
-    //   if (err) throw err
-    //   //if inserted then ok
-    // })
-    //!!!!!!!!!!!!TO-DO will we use groupnaem or group_ID as a socket room????!!!!!!!!!!!!
-    // io.to(msg.groupID).emit('sendMessage', msg.username + " : " + msg.message)
+    connection.query(query, [msg.message, mysqlTimestamp, msg.userName, msg.groupName], function (err, results) {
+      if (err) throw err
+      //if inserted then ok
+      console.log(result);
+      callback({status:"SUCCESS", result:result});
+      console.log('send message success!!!');
+    })
 
 
   })
@@ -252,7 +255,6 @@ io.on('connection', function (socket) {
 
   socket.on('disconnect', function () {
     console.log('client disconnect...', socket.id)
-    handleDisconnect()
   })
 
   socket.on('error', function (err) {
